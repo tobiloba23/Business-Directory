7c203dbe400e7e331e5272555ca4f39d4ebe1d4f
tobiloba23 <tobiloba.ades@gmail.com>
2018-03-07 12:46:55 +0100

    chore: added eslint rules for client app

---
+++ client/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends":[
+    "airbnb",
+    "react-app"
+  ],
+  "globals": {
+    "window": true,
+    "document": true,
+    "localStorage": true
+  },
+  "rules": {
+  }
+};
